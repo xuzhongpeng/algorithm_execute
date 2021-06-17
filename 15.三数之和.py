623c6d9ba9f92fb1ce3dc3e3e440d3899fddbe7c
xuzhongpeng <1452754335@qq.com>
2021-06-17 22:50:24 +0800

    note-sync syncing

---
+++ 15.三数之和.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=15 lang=python3
+#
+# [15] 三数之和
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        print('')
+        
+# @lc code=end
+
+Solution().threeSum([-1,0,1,2,-1,-4])
