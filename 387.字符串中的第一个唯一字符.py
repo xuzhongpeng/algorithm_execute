5ce147671a21d1abe427d8cd0ba70f79b8baf07f
xuzhongpeng <1452754335@qq.com>
2021-06-23 22:15:48 +0800

    note-sync syncing

--- 387.字符串中的第一个唯一字符.py
+++ 387.字符串中的第一个唯一字符.py
@@ -7,5 +7,18 @@
 # @lc code=start
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        m = {}
+        for (i,v) in enumerate(s):
+            # print(i)
+            if v in m:
+                m[v].append(i)
+            else:
+                m[v] = [i]
+        result = len(s)
+        for x in m.values():
+            if len(x) == 1 and x[0] < result:
+                result = x[0]
+        return -1 if result == len(s) else result
 # @lc code=end
 
+print(Solution().firstUniqChar('leetcode'))
