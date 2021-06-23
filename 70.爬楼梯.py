8f6fa6fc92369bef6dae20c33aee29ada68e51f1
xuzhongpeng <1452754335@qq.com>
2021-06-23 23:58:12 +0800

    note-sync syncing

--- 70.爬楼梯.py
+++ 70.爬楼梯.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
+        def p(f):
+            if f==1: return 1
+            elif f == 2: return 2
+            return p(f-1)+2
+        return p(n)
 # @lc code=end
 
 print(Solution().climbStairs(5))
