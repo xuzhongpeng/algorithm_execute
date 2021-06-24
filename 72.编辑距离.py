95c81e95c4ce35fd945febcdeecb74d75090a9ec
xuzhongpeng <1452754335@qq.com>
2021-06-24 22:44:11 +0800

    note-sync syncing

---
+++ 72.编辑距离.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=72 lang=python3
+#
+# [72] 编辑距离
+#
+
+# @lc code=start
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+# @lc code=end
+
+做不来啊，没办法啊，下次再做啊
