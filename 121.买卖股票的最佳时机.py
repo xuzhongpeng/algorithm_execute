65a4573d0c03a4ed4e154c633f33102d22fc885b
xuzhongpeng <1452754335@qq.com>
2021-06-24 22:54:50 +0800

    note-sync syncing

--- 121.买卖股票的最佳时机.py
+++ 121.买卖股票的最佳时机.py
@@ -5,7 +5,15 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        a = 0
+        b = 0
+        for p in prices:
+            a = min()
 # @lc code=end
-
+s= Solution()
+print(s.maxProfit([7,1,5,3,6,4]))
