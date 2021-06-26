be5a05cc2abf2c7eb1d230099a3d55d56680d734
xuzhongpeng <1452754335@qq.com>
2021-06-26 14:01:47 +0800

    note-sync syncing

--- 刷题指南/高考zhiyuan.py
+++ 刷题指南/高考zhiyuan.py
@@ -9,15 +9,15 @@ headers={
 #未来提高代码的效率，没有对输入内容进行判断
 major = input("请查询输入专业：")
 
-times = input("请输入查询年份(2014~2018)：")
+times = 2020 #input("请输入查询年份(2014~2018)：")
 
 for i in range(32,968,1):#学校id从[32,967),省略前面部分可以节约时间
   url = f'https://api.eol.cn/gkcx/api/?access_token=&local_province_id=52&local_type_id=1&page=1&school_id={i}&signsafe=&size=20&uri=apidata/api/gk/score/special&year='+str(times)
+  url = f'https://static-data.eol.cn/www/2.0/schoolspecialindex/{str(times)}/{i}/50/1/7/1.json'
   time.sleep(0.1) #暂停时间模拟人为请求
   res = requests.post(url=url,headers=headers)
-  if res.json()['md5'] !='F03C3B1E12B97478ED80E3868E2EEA94': #字典不为空再继续执行下面条件语句，提高效率
-    for items in res.json()['data']['item']:
-      if('spname' in items): #存在spname才执行，如西南科技大学
-        if (items['spname'] == major):
-          print(items['name'],'\t',str(times)+"年"+str(major)+"录取最高分",items['max'],'\t',"平均分：",items['average'],'\t',"最低分：",items['min'],'\t',"最低位次:",items['min_section'],'\t',"录取批次:",items['local_batch_name'],'\n')
+  for items in res.json()['data']['item']:
+    if('spname' in items): #存在spname才执行，如西南科技大学
+      if (items['spname'] == major):
+        print(items['name'],'\t',str(times)+"年"+str(major)+"录取最高分",items['max'],'\t',"平均分：",items['average'],'\t',"最低分：",items['min'],'\t',"最低位次:",items['min_section'],'\t',"录取批次:",items['local_batch_name'],'\n')
 print(str(times)+"年全国"+str(major)+"专业录取信息查询完成！") #所有数据遍历完成后才会打印它
